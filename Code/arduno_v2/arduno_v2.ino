dc111bbfe2d627935c60214ffa6b1c394d74b73e
Nguyen Quang Nhut Quynh <ngquangnhutquynh@gmail.com>
2021-06-03 13:09:06 +0700

    Đã có thể truyền dữ liệu từ esp sang arduino trên V2

--- Code/arduno_v2/arduno_v2.ino
+++ Code/arduno_v2/arduno_v2.ino
@@ -1,41 +1,21 @@
+
 #include <SoftwareSerial.h>
-#include <SerialCommand.h>
 
-#define _ESP_RX 2;
-#define _ESP_TX 3;
+SoftwareSerial espSerial(2, 3);
 
-const long int Baudrate = 9600;
-
-SoftwareSerial espSerial(_ESP_RX, _ESP_TX);
-SerialCommand espCmd(espSerial);
-
-void setup()
-{
-  Serial.begin(Baudrate);
-  espSerial.begin(Baudrate);
-  espCmd.addCommand("TEMPERATURE", GetTemperature);
+void setup(){
+  Serial.begin(9600);
+  espSerial.begin(4800);
 }
 
-unsigned long chuky1 = 0;
-const unsigned long CHU_KY_1_LA_BAO_NHIEU = 3000;
-
-void loop()
-{
-  if (millis() - chuky1 > CHU_KY_1_LA_BAO_NHIEU)
-  {
-    chuky1 = millis();
-    GetTemperature();
+void loop(){
+  while(espSerial.available()>0){
+//    String val = (String) espSerial.readString();
+////    if(espSerial.read() == '\n'){
+////      Serial.println(val);
+////    }
+//    Serial.println(val);
+    Serial.println(Serial.read());
   }
-
-  sCmd.readSerial();
-}
-
-void GetTemperature()
-{
-  espCmd.print("TEMPERATURE");
-  espCmd.print('\r');
-  espCmd.print('a');
-  espCmd.print('\r');
-
-  Serial.println("Da chay den day");s
+  delay(30);
 }
--- Code/esp_v2/esp_v2.ino
+++ Code/esp_v2/esp_v2.ino
@@ -1,76 +1,17 @@
-#include <ArduinoJson.h>
 #include <SoftwareSerial.h>
-#include <SerialCommand.h>
 #include <ESP8266WiFi.h>
-#include <SocketIOClient.h>
-#include <String.h>
 
-#define RX 1;
-#define TX 2;
+SoftwareSerial arduinoSerial(D1,D2);
 
-const long int Baudrate = 9600;
-
-SoftwareSerial mySerial(RX, TX, false, 256);
-SerialCommand sCmd(mySerial;)
-
-SocketIOClient client;
-
-// const char* ssid = "BC22QN";
-// const char* password = "300736253";
-char host[] = "192.168.10.22";
-
-const char* ssid = "C202";
-const char* password = "ciscoc202";
-char host[] = "192.168.10.45";
-
-int port = 3484;
-char namespace_esp8266[] = "esp8266";
-
-void defaultCommand(String command){
-  char *json = sCmd.next();
-  client.send(command, (String) json);  //Gửi dữ liệu về cho Socket Server
-  Serial.print("--------------");
-  //In ra serial monitor để debug
-  Serial.print(command);
-  Serial.print(' ');
-  Serial.println(json);
+void setup(){
+  Serial.begin(9600);
+  arduinoSerial.begin(4800);
+  pinMode(D1, INPUT);
+  pinMode(D2, OUTPUT);
 }
 
-void setup() {
-  Serial.begin(Baudrate);
-  mySerial.begin(Baudrate);
-  dely(500);
-
-  Serial.print("WiFi Connecting: ");
-  Serial.print(ssid);
-
-  WiFi.begin(ssid, password);
-  while (Wifi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println();
-  Serial.println("WiFi Connected");
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());
-  
-  if (!client.connect(host, port, namespace_esp8266)){
-    Serial.println("Failed to connect to server");
-    return;
-  }
-
-  sCmd.addDefaultHandler(defaultCommand);
-  Serial.println("Ready");
-}
-
-void loop() {
-
-  if (!client.connected()){
-    client.reconnect(host, port, namespace_esp8266);
-  }
-  // sCmd.readSerial();
-  String serialString = mySerial.readString();
-  Serial.println(serialString);
+void loop(){
+  arduinoSerial.println("Hello");
+  Serial.println("Hello");
+  delay(30);
 }
